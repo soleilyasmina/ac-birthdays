fd75bdf00040d5591ae8ce9c57e97b63aec874db
soleilyasmina <mounlasolomon@gmail.com>
2020-04-13 21:01:45 -0400

    successfully created signature

--- index.js
+++ index.js
@@ -12,8 +12,6 @@ const {
   TWITTER_TOKEN_SECRET,
 } = process.env;
 
-
-
 // collect parameters: if media, no include_entities or status
 // include_entities: true, oauth_consumer_key, oauth_nonce,
 // oauth_signature_method: HMAC-SHA1, oauth_timestamp,
@@ -23,12 +21,9 @@ const {
 
 const collectParameters = (isMedia, status) => {
   const nonce = crypto.randomBytes(32).toString('base64');
-  const parameterString = `${isMedia ? null : 'include_entities=true&'}oauth_consumer_key=${TWITTER_CONSUMER_KEY}&oauth_consumer_secret=${TWITTER_CONSUMER_SECRET}&oauth_nonce=${nonce}&oauth_signature_method=HMAC-SHA1&oauth_timestamp=${new Date().getTime()}&oauth_token=${TWITTER_TOKEN}&oauth_token_secret=${TWITTER_TOKEN_SECRET}&oauth_version=1.0${isMedia ? null : `&status=${encodeURIComponent(status).replace('!', 
-    '%21')}`}`;
+  const parameterString = `${isMedia ? '' : 'include_entities=true&'}oauth_consumer_key=${TWITTER_CONSUMER_KEY}&oauth_consumer_secret=${TWITTER_CONSUMER_SECRET}&oauth_nonce=${nonce}&oauth_signature_method=HMAC-SHA1&oauth_timestamp=${new Date().getTime()}&oauth_token=${TWITTER_TOKEN}&oauth_token_secret=${TWITTER_TOKEN_SECRET}&oauth_version=1.0${isMedia ? '' : `&status=${encodeURIComponent(status).replace('!', '%21')}`}`;
   return parameterString;
 };
-
-
 
 // signature base string:
 // method: POST
@@ -36,14 +31,28 @@ const collectParameters = (isMedia, status) => {
 // percent encoded parameter string
 // delimit these with &
 
-// signing key:
-// consumer secret + oauth token secret
-// percent encode both, delimit with &
+const createBaseString = (isMedia, parameterString) => (
+  !isMedia
+    ? `POST&=${encodeURIComponent('https://api.twitter.com/1.1/statuses/update.json')}&${encodeURIComponent(parameterString)}`
+    : `${encodeURIComponent('https://api.twitter.com/1,1/media/upload.json')}&${encodeURIComponent(parameterString)}`
+);
 
 // create signature:
 // use const hmac = crypto.createHmac('sha1', signingKey);
 // hmac.update(signatureBaseString);
 // const signature = hmac.digest('base64');
+
+const createSignature = (isMedia, status) => {
+  const signingKey = `${encodeURIComponent(TWITTER_CONSUMER_SECRET)}&${encodeURIComponent(TWITTER_TOKEN_SECRET)}`;
+  const parameterString = collectParameters(isMedia, status);
+  const base = createBaseString(isMedia, parameterString);
+  const hmac = crypto.createHmac('sha1', signingKey);
+  hmac.update(base);
+  const signature = hmac.digest('base64');
+  return signature;
+};
+
+console.log(createSignature(false, 'Hey, friends!'));
 
 // make Nookipedia API Call
 // assign villager_images to villagerImages
